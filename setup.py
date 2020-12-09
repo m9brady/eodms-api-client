c24a31bc0770468e03ceb97a0cafca5172c29a29
Mike Brady <5256866+m9brady@users.noreply.github.com>
2020-12-09 10:52:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,24 @@ version = re.search(r"__version__ = \s*'([\d.*]+)'", init_txt).group(1)
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+    
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
 
 setup(
     name=name,
     version=version,
+    author="Mike Brady",
+    author_email="mike.brady(at)canada.ca",
     description='Tool for querying and submitting image orders to Natural Resources Canada\'s Earth Observation Data Management System (EODMS)',
+    long_description=long_description,
     packages=find_packages(exclude=['docs']),
     install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='~=3.6',
     entry_points='''
     [console_scripts]
